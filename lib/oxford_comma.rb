9d4c97ccbf41608159d7b8695e9f88a7199bb1dd
Trish Ogen <patriciaogen@gmail.com>
2019-05-17 01:03:36 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,8 @@
 def oxford_comma(array)
   if array.length == 1
     array[0]
+  elsif array.length == 2
+    array[0] + "and" + array[1]
   else
     last_element = array.pop
     array.join(", ") << "and" << last_element
