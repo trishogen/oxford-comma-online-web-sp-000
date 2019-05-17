e228fb864357ceeac01f1bdc5ce751268dc9c720
Trish Ogen <patriciaogen@gmail.com>
2019-05-17 01:04:58 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    array[0]
+  elsif array.length == 2
+    array[0] + " and " + array[1]
+  else
+    last_element = array.pop
+    array.join(", ") << ", and " << last_element
+  end
 end
